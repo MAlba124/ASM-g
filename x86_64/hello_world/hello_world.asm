9f090efc8eb496ecfe8a2811372fdfe12d136cdb
MAlba124 <0x583po@protonmail.com>
2022-07-11 20:05:00 +0200

    Hello world

---
+++ x86_64/hello_world/hello_world.asm
@@ -0,0 +1,17 @@
+    global _start
+
+    section .data
+hello_world:
+    db "Hello World", 0xA  ; "Hello World\n"
+
+    section .text
+_start:
+    mov rax, 1                  ; write
+    mov rdi, 1                  ; stdout
+    mov rsi, hello_world        ; hello world text
+    mov rdx, 12                 ; message size/length
+    syscall
+
+    mov rax, 60                 ; exit
+    mov rdi, 0                  ; error code
+    syscall
